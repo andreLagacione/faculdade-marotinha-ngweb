979ae906d1f7d73bf70899cf64eda66e1dbf0f02
andreLagacione <lagacioneandre@gmail.com>
2019-12-03 20:15:47 -0300

    add base services

---
+++ src/app/shared/services/control-elements.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, Injector, EventEmitter } from '@angular/core';
+
+// services
+import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
+import { BaseResourceModel } from 'src/app/shared/models/base-resource.model';
+
+@Injectable()
+export class ControlElementsService extends BaseResourceService<BaseResourceModel> {
+	public loaderControl$: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+	constructor(
+		public injector: Injector
+	) {
+		super('', injector);
+	}
+
+	public loaderControl(show: boolean) {
+		this.loaderControl$.emit(show);
+	}
+}
