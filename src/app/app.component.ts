6833a0e3b38909c4d72bbb2563cdd9fcf20114e1
andreLagacione <lagacioneandre@gmail.com>
2019-12-03 20:12:11 -0300

    create main application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+// services
+import { ControlElementsService } from './shared/services/control-elements.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'faculdade-marotinha-ngweb';
+  private unsubscribe$: Subject<void> = new Subject<void>();
+	public toggleMenu = false;
+	public showLoader = false;
+
+  constructor(
+    private titleService: Title,
+    private controlElementsService: ControlElementsService
+  ) {
+    this.controlElementsService.loaderControl$
+			.pipe(
+				takeUntil(this.unsubscribe$)
+			)
+			.subscribe(
+				_response => this.showLoader = _response
+			);
+  }
+
+  ngOnInit() {
+    this.titleService.setTitle('Home | Faculdade Marotinha');
+  }
+
+	ngOnDestroy() {
+		this.unsubscribe$.next();
+		this.unsubscribe$.complete();
+	}
 }
