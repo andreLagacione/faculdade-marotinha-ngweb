401f8f087832bfbef5e29263299e25935302c3b1
andreLagacione <lagacioneandre@gmail.com>
2019-12-03 20:13:18 -0300

    add dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule, Title } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { registerLocaleData, LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import localePt from '@angular/common/locales/pt';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // modules
 import { ToastrModule } from 'ngx-toastr';
@@ -10,9 +11,13 @@ import { ToastrModule } from 'ngx-toastr';
 // routes
 import { AppRoutingModule } from './app-routing.module';
 
+// services
+import { ControlElementsService } from './shared/services/control-elements.service';
+
 // components
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { HttpClientModule } from '@angular/common/http';
 
 registerLocaleData(localePt);
 
@@ -25,10 +30,14 @@ registerLocaleData(localePt);
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+		ReactiveFormsModule,
     ToastrModule.forRoot(),
   ],
   providers: [
     Title,
+    ControlElementsService,
     {
       provide: localePt,
       useValue: 'pt-BR'
@@ -38,6 +47,7 @@ registerLocaleData(localePt);
       useClass: HashLocationStrategy
     }
   ],
+	schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
