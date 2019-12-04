21068080fe96fde88da0545156f0989e040ac4fd
andreLagacione <lagacioneandre@gmail.com>
2019-12-04 14:38:55 -0300

    create aluno module

---
+++ src/app/aluno/aluno.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// components
+import { ListComponent } from './component/list/list.component';
+
+@NgModule({
+	declarations: [ListComponent],
+	imports: [
+		CommonModule
+	]
+})
+export class AlunoModule { }
