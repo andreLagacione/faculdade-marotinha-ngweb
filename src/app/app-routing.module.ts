f4a6fe53842e15e563f5858c70ec9bcc0ab1cad8
andreLagacione <lagacioneandre@gmail.com>
2019-12-04 14:40:09 -0300

    map routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// components
+import { ListComponent } from './aluno/component/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+		path: '',
+		redirectTo: 'alunos',
+		pathMatch: 'full'
+	}, {
+		path: 'alunos',
+		component: ListComponent
+	},
+
+
+	{
+		path: '**',
+		component: ListComponent
+	}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
