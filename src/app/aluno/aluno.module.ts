d8c50c1980f496283954332dc6e92a51ca456e89
andreLagacione <lagacioneandre@gmail.com>
2019-12-04 17:57:24 -0300

    import modules and pipes

--- src/app/aluno/aluno.module.ts
+++ src/app/aluno/aluno.module.ts
@@ -1,13 +1,39 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// modules
+import { ButtonsModule } from '../buttons/buttons.module';
+import { ToasterModule } from '../toaster/toaster.module';
+import { PaginationModule } from '../pagination/pagination.module';
+import { ConfirmModalModule } from '../confirm-modal/confirm-modal.module';
+import { SidebarFilterModule } from '../sidebar-filter/sidebar-filter.module';
+
+// pipes
+import { ToIntPipe } from '../shared/pipes/to-int.pipe';
+import { CpfCnpjPipe } from '../shared/pipes/cpf-cnpj.pipe';
+import { FonePipe } from '../shared/pipes/telefone.pipe';
 
 // components
 import { ListComponent } from './component/list/list.component';
 
 @NgModule({
-	declarations: [ListComponent],
+	declarations: [
+		ListComponent,
+		ToIntPipe,
+		CpfCnpjPipe,
+		FonePipe
+	],
 	imports: [
-		CommonModule
-	]
+		CommonModule,
+		ButtonsModule,
+		FormsModule,
+		ReactiveFormsModule,
+		ToasterModule.forRoot(),
+		PaginationModule.forRoot(),
+		ConfirmModalModule.forRoot(),
+		SidebarFilterModule.forRoot(),
+	],
+	schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AlunoModule { }
