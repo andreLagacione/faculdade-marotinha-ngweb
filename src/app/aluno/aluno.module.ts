dfa1cb731a730daf620826f0fa5bae530cfad8ee
andreLagacione <lagacioneandre@gmail.com>
2019-12-05 22:40:49 -0300

    inject new lib

--- src/app/aluno/aluno.module.ts
+++ src/app/aluno/aluno.module.ts
@@ -8,6 +8,7 @@ import { ToasterModule } from '../toaster/toaster.module';
 import { PaginationModule } from '../pagination/pagination.module';
 import { ConfirmModalModule } from '../confirm-modal/confirm-modal.module';
 import { SidebarFilterModule } from '../sidebar-filter/sidebar-filter.module';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 // pipes
 import { ToIntPipe } from '../shared/pipes/to-int.pipe';
@@ -16,13 +17,15 @@ import { FonePipe } from '../shared/pipes/telefone.pipe';
 
 // components
 import { ListComponent } from './component/list/list.component';
+import { CreateComponent } from './component/create/create.component';
 
 @NgModule({
 	declarations: [
 		ListComponent,
 		ToIntPipe,
 		CpfCnpjPipe,
-		FonePipe
+		FonePipe,
+		CreateComponent
 	],
 	imports: [
 		CommonModule,
@@ -33,6 +36,7 @@ import { ListComponent } from './component/list/list.component';
 		PaginationModule.forRoot(),
 		ConfirmModalModule.forRoot(),
 		SidebarFilterModule.forRoot(),
+		NgSelectModule,
 	],
 	schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
