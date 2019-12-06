8cb18b4b20e26cc50515f67dbd863164cc4f0ee2
andreLagacione <lagacioneandre@gmail.com>
2019-12-05 22:39:07 -0300

    remove import not used and change model

--- src/app/aluno/component/list/list.component.ts
+++ src/app/aluno/component/list/list.component.ts
@@ -3,15 +3,14 @@ import { Component, OnInit, Injector } from '@angular/core';
 // services
 import { ControlElementsService } from 'src/app/shared/services/control-elements.service';
 import { BaseResouceListComponent } from 'src/app/shared/components/base-resource-list.component';
-import { AlunoListModel } from '../../models/aluno-list.model';
+import { AlunoModel } from '../../models/aluno.model';
 import { AlunoService } from '../../services/aluno.service';
 
 @Component({
 	selector: 'app-list',
-	templateUrl: './list.component.html',
-	styleUrls: ['./list.component.scss']
+	templateUrl: './list.component.html'
 })
-export class ListComponent extends BaseResouceListComponent<AlunoListModel> implements OnInit {
+export class ListComponent extends BaseResouceListComponent<AlunoModel> implements OnInit {
 
 	constructor(
 		protected injector: Injector,
