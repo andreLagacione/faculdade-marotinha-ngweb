5784061a2d39f45896485c0a94562f52004723ea
andreLagacione <lagacioneandre@gmail.com>
2019-12-06 10:09:11 -0300

    edit aluno

--- src/app/aluno/component/create/create.component.ts
+++ src/app/aluno/component/create/create.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Injector } from '@angular/core';
+import { Component, OnInit, Injector, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 
@@ -12,13 +12,14 @@ import { ControlElementsService } from 'src/app/shared/services/control-elements
 
 // components
 import { BaseResourceRegisterComponent } from 'src/app/shared/components/base-resource-register.component';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
 	selector: 'app-create',
 	templateUrl: './create.component.html',
 	styleUrls: ['./create.component.scss']
 })
-export class CreateComponent extends BaseResourceRegisterComponent<AlunoModel> implements OnInit {
+export class CreateComponent extends BaseResourceRegisterComponent<AlunoModel> implements OnInit, OnDestroy {
 	public cursosList: Observable<CursoModel[]>;
 	public selectedCursos: CursoModel[] = [];
 
@@ -28,6 +29,14 @@ export class CreateComponent extends BaseResourceRegisterComponent<AlunoModel> i
 		private controlElementsService: ControlElementsService
 	) {
 		super(injector, alunoService);
+
+		this.resourceService.updateFormValues$
+			.pipe(
+				takeUntil(this.unsubscribe$)
+			)
+			.subscribe(
+				_response => this.patchaValuesForm(_response)
+			);
 	}
 
 	ngOnInit() {
@@ -37,14 +46,20 @@ export class CreateComponent extends BaseResourceRegisterComponent<AlunoModel> i
 		this.backRoute = '/alunos';
 
 		if (this.regiterId) {
+			this.getById();
 			this.controlElementsService.pageName('Editar aluno');
 		}
+	}
+
+	ngOnDestroy() {
+		this.unsubscribe$.next();
+		this.unsubscribe$.complete();
 	}
 
 	private instaciateForm() {
 		this.registerForm = new FormGroup({
 			name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
-			age: new FormControl('', [Validators.required, Validators.min(15), Validators.maxLength(150)]),
+			age: new FormControl('', [Validators.required, Validators.min(15), Validators.max(150)]),
 			cpf: new FormControl('', [Validators.required]),
 			phone: new FormControl('', [Validators.required])
 		});
@@ -62,7 +77,8 @@ export class CreateComponent extends BaseResourceRegisterComponent<AlunoModel> i
 
 	public save() {
 		const values = this.registerForm.value;
-		const curso: AlunoModel = {
+		const aluno: AlunoModel = {
+			id: this.regiterId,
 			name: values.name,
 			age: values.age,
 			cpf: values.cpf,
@@ -70,10 +86,25 @@ export class CreateComponent extends BaseResourceRegisterComponent<AlunoModel> i
 			cursos: this.selectedCursos
 		};
 
-		super.create(curso);
+		if (this.regiterId) {
+			super.update(aluno);
+			return false;
+		}
+
+		super.create(aluno);
 	}
 
+	private patchaValuesForm(values: object) {
+		this.selectedCursos = values['cursoNomeListaDTOList'];
+		this.registerForm.patchValue({
+			name: values['name'],
+			age: values['age'],
+			cpf: values['cpf'],
+			phone: values['phone']
+		});
 
+		this.registerForm.markAsDirty();
+	}
 
 	get name() {
 		return this.registerForm.get('name');
