3ea1d8fad8f1c5e7ac9f725c194faf23f23c67e1
andreLagacione <lagacioneandre@gmail.com>
2019-12-05 22:40:01 -0300

    add new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 // components
 import { ListComponent } from './aluno/component/list/list.component';
+import { CreateComponent } from './aluno/component/create/create.component';
 
 const routes: Routes = [{
 		path: '',
@@ -11,6 +12,12 @@ const routes: Routes = [{
 	}, {
 		path: 'alunos',
 		component: ListComponent
+	}, {
+		path: 'alunos/cadastrar',
+		component: CreateComponent
+	}, {
+		path: 'alunos/editar/:id',
+		component: CreateComponent
 	},
 
 
