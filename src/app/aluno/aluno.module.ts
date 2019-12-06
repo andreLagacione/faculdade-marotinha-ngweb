9b001bae5822531d9153a1674d3846062c04398b
andreLagacione <lagacioneandre@gmail.com>
2019-12-06 18:30:03 -0300

    organize dependencies and inject shared module

--- src/app/aluno/aluno.module.ts
+++ src/app/aluno/aluno.module.ts
@@ -1,19 +1,8 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // modules
-import { ButtonsModule } from '../buttons/buttons.module';
-import { ToasterModule } from '../toaster/toaster.module';
-import { PaginationModule } from '../pagination/pagination.module';
-import { ConfirmModalModule } from '../confirm-modal/confirm-modal.module';
-import { SidebarFilterModule } from '../sidebar-filter/sidebar-filter.module';
-import { NgSelectModule } from '@ng-select/ng-select';
-
-// pipes
-import { ToIntPipe } from '../shared/pipes/to-int.pipe';
-import { CpfCnpjPipe } from '../shared/pipes/cpf-cnpj.pipe';
-import { FonePipe } from '../shared/pipes/telefone.pipe';
+import { SharedModule } from '../shared/shared.module';
 
 // components
 import { ListComponent } from './component/list/list.component';
@@ -22,21 +11,11 @@ import { CreateComponent } from './component/create/create.component';
 @NgModule({
 	declarations: [
 		ListComponent,
-		ToIntPipe,
-		CpfCnpjPipe,
-		FonePipe,
 		CreateComponent
 	],
 	imports: [
 		CommonModule,
-		ButtonsModule,
-		FormsModule,
-		ReactiveFormsModule,
-		ToasterModule.forRoot(),
-		PaginationModule.forRoot(),
-		ConfirmModalModule.forRoot(),
-		SidebarFilterModule.forRoot(),
-		NgSelectModule,
+		SharedModule
 	],
 	schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
--- src/app/curso/curso.module.ts
+++ src/app/curso/curso.module.ts
@@ -1,12 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// modules
+import { SharedModule } from '../shared/shared.module';
 
+// components
+import { CursoListComponent } from './component/curso-list/curso-list.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+	declarations: [
+		CursoListComponent,
+	],
+	imports: [
+		CommonModule,
+		SharedModule
+	],
+	schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class CursoModule { }
