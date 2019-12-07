9bb5af146137f825f3dc6a26188d0cf21e99fd2b
andreLagacione <lagacioneandre@gmail.com>
2019-12-07 19:26:39 -0300

    inject module, component and add custom schema

--- src/app/professor/professor.module.ts
+++ src/app/professor/professor.module.ts
@@ -1,12 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// module
+import { SharedModule } from '../shared/shared.module';
 
+// components
+import { ProfessorListComponent } from './component/professor-list/professor-list.component';
 
 @NgModule({
-	declarations: [],
+	declarations: [ProfessorListComponent],
 	imports: [
-		CommonModule
-	]
+		CommonModule,
+		SharedModule
+	],
+	schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ProfessorModule { }
