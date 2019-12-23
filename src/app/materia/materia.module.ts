69524d008cfb8162c0819cb35e1d2755f7226ebc
André Lagacione <lagacioneandre@gmail.com>
2019-12-23 11:45:48 -0300

    import SharedModule and configuration

--- src/app/materia/materia.module.ts
+++ src/app/materia/materia.module.ts
@@ -1,14 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// modules
+import { SharedModule } from '../shared/shared.module';
+
+// components
 import { MateriaCreateComponent } from './component/materia-create/materia-create.component';
 import { MateriaListComponent } from './component/materia-list/materia-list.component';
 
-
-
 @NgModule({
-	declarations: [MateriaCreateComponent, MateriaListComponent],
+	declarations: [
+		MateriaCreateComponent,
+		MateriaListComponent
+	],
 	imports: [
-		CommonModule
-	]
+		CommonModule,
+		SharedModule
+	],
+	schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class MateriaModule { }
