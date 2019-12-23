798fbfeefa65f9a018d1ada88727f275175b0ffd
André Lagacione <lagacioneandre@gmail.com>
2019-12-23 11:47:01 -0300

    create materia list

--- src/app/materia/component/materia-list/materia-list.component.ts
+++ src/app/materia/component/materia-list/materia-list.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injector } from '@angular/core';
+import { BaseResouceListComponent } from 'src/app/shared/components/base-resource-list.component';
+import { MateriaModel } from '../../models/materia.model';
+import { MateriaService } from '../../services/materia.service';
+import { ControlElementsService } from 'src/app/shared/services/control-elements.service';
 
 @Component({
   selector: 'app-materia-list',
-  templateUrl: './materia-list.component.html',
-  styleUrls: ['./materia-list.component.scss']
+  templateUrl: './materia-list.component.html'
 })
-export class MateriaListComponent implements OnInit {
+export class MateriaListComponent extends BaseResouceListComponent<MateriaModel> implements OnInit {
 
-  constructor() { }
+  constructor(
+		protected injector: Injector,
+		protected materiaService: MateriaService,
+		private controlElementsService: ControlElementsService
+	) {
+		super(injector, materiaService);
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.registerRoute = '/materia/cadastrar';
+		this.editRoute = '/materia/editar';
+		this.controlElementsService.pageName('Materias');
+		this.getAllPageable();
+	}
+
+	protected getAllPageable() {
+		this.baseParamsPage = `?sort=name,asc&size=${this.pageSize}&page=${this.currentPage}`;
+		super.getAllPageable();
+	}
 
 }
