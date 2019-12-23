db20e71e12525c498363d61e85cd6512d9e93940
André Lagacione <lagacioneandre@gmail.com>
2019-12-23 11:30:10 -0300

    create materia create component

---
+++ src/app/materia/component/materia-create/materia-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-materia-create',
+  templateUrl: './materia-create.component.html',
+  styleUrls: ['./materia-create.component.scss']
+})
+export class MateriaCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
