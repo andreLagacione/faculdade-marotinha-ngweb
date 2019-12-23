6f3d55af80299075d49607ec578b8b72a5c54c2d
André Lagacione <lagacioneandre@gmail.com>
2019-12-23 11:30:44 -0300

    inject new components in module

--- src/app/materia/materia.module.ts
+++ src/app/materia/materia.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MateriaCreateComponent } from './component/materia-create/materia-create.component';
+import { MateriaListComponent } from './component/materia-list/materia-list.component';
 
 
 
 @NgModule({
-	declarations: [],
+	declarations: [MateriaCreateComponent, MateriaListComponent],
 	imports: [
 		CommonModule
 	]
