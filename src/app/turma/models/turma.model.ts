ed72e880ee6cb2a0df8663c35e2f800322baf74d
andreLagacione <lagacioneandre@gmail.com>
2019-12-23 22:31:39 -0300

    create truma module

---
+++ src/app/turma/models/turma.model.ts
@@ -0,0 +1,15 @@
+// models
+import { BaseResourceModel } from 'src/app/shared/models/base-resource.model';
+
+export class TurmaModel extends BaseResourceModel {
+	ano?: number;
+	curso?: string;
+	professor?: string;
+	totalAlunos?: number;
+	periodo?: string;
+	alunos?: string[];
+
+	constructor() {
+		super();
+	}
+}
